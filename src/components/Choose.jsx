e573d0ecfe7ba21736a4ecb9cb57379a3d8eb817
karnika gupta <karnikagupta1830@gmail.com>
2023-05-30 17:31:08 +0530

    fixed margins

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -13,46 +13,46 @@ function Choose() {
         </p>
         <div className='lg:flex mt-[38px]'>
             <div className=''>
-                <div className='bg-[#3461FF] lg:ml-[60px] ml-[150px] rounded-full w-[150px] h-[150px]'>
+                <div className='bg-[#3461FF] lg:ml-[50px] ml-[150px] rounded-full w-[150px] h-[150px]'>
                     <img src={quality} alt="" className='p-4 pt-12 pl-7'/>
                 </div>
-                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] lg:ml-[20px] ml-[110px] w-[230px] h-[170px] pt-16 pl-5'>
+                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] lg:ml-[20px] ml-[110px] w-[200px] h-[170px] pt-16 pl-2'>
                     <p className='text-lg font-display1'>High Quality Leads</p>
-                    <p className='pt-2 text-sm w-[196px] h-[54px]'>All Based Targeting For 30% More Qualifies Leads</p>
+                    <p className='text-sm w-[196px] h-[54px]'>All Based Targeting For 30% More Qualifies Leads</p>
                 </div>
             </div>
             <div className='lg:mt-[100px] mt-[40px]'>
-                <div className='bg-[#3461FF] lg:ml-[50px] ml-[150px] rounded-full w-[150px] h-[150px]'>
+                <div className='bg-[#3461FF] lg:ml-[40px] ml-[150px] rounded-full w-[150px] h-[150px]'>
                     <img src={cpl} alt="" className='p-4 pt-6 pl-7'/>
                 </div>
-                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[230px] h-[170px] pt-16 pl-5'>
+                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[200px] h-[170px] pt-16 pl-2'>
                     <p className='text-lg font-display1'>Reduced CPL</p>
                     <p className='pt-2 text-sm w-[196px] h-[54px]'>All Based Targeting For 30% More Qualifies Leads</p>
                 </div>
             </div>
             <div className='lg:mt-[200px] mt-[40px]'>
-                <div className='bg-[#3461FF] lg:ml-[50px] ml-[150px] rounded-full w-[150px] h-[150px]'>
+                <div className='bg-[#3461FF] lg:ml-[40px] ml-[150px] rounded-full w-[150px] h-[150px]'>
                     <img src={inhouse} alt="" className='p-4 pt-9 pl-8'/>
                 </div>
-                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[230px] h-[170px] pt-16 pl-5'>
+                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[200px] h-[170px] pt-16 pl-2'>
                     <p className='text-lg font-display1'>In-House Algorithm</p>
                     <p className='pt-2 text-sm w-[196px] h-[54px]'>Proven inhouse algorithms based on geo targeting</p>
                 </div>
             </div>
             <div className='lg:mt-[100px] mt-[40px]'>
-                <div className='bg-[#3461FF] lg:ml-[50px] ml-[150px] rounded-full w-[150px] h-[150px]'>
+                <div className='bg-[#3461FF] lg:ml-[40px] ml-[150px] rounded-full w-[150px] h-[150px]'>
                     <img src={sales} alt="" className='p-4 pt-9 pl-9'/>
                 </div>
-                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[230px] h-[170px] pt-16 pl-5'>
+                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[200px] h-[170px] pt-16 pl-2'>
                     <p className='text-lg font-display1'>Sales Cycle</p>
                     <p className='pt-2 text-sm w-[196px] h-[54px]'>Shorten your sale cycle by 2x with our data driven solutions </p>
                 </div>
             </div>
             <div className='lg:mt-[0px] mt-[40px]'>
-                <div className='bg-[#3461FF] lg:ml-[50px] ml-[150px] rounded-full w-[150px] h-[150px]'>
+                <div className='bg-[#3461FF] lg:ml-[40px] ml-[150px] rounded-full w-[150px] h-[150px]'>
                     <img src={transparent} alt="" className='p-4 pt-12 pl-12'/>
                 </div>
-                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[230px] h-[170px] pt-16 pl-5'>
+                <div className='rounded-xl shadow-slate-700 shadow-2xl mt-[-40px] ml-[110px] lg:ml-[10px] w-[200px] h-[170px] pt-16 pl-2'>
                     <p className='text-lg font-display1'>100% Transparency</p>
                     <p className='pt-2 text-sm w-[196px] h-[54px]'>lead security with our bit to bit encryption</p>
                 </div>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -14,7 +14,7 @@ function Partners() {
         </p>
         <div className='mt-[38px] bg-[#F3F6FF] lg:w-[1840px] md:w-[1024px] lg:ml-[-400px] md:ml-0 lg:overflow-auto md:overflow-scroll' >
             <div className='flex md:w-[1024px] m-auto md:overflow-scroll lg:overflow-auto overflow-scroll'>
-              <img className='w-[263px] h-[116px] mt-12 lg:ml-[75px]' src={m3m} alt="" />
+              <img className='w-[263px] h-[116px] mt-12 ' src={m3m} alt="" />
               <img className='w-[263px] h-[116px] mt-12 ml-[34px]' src={whiteland} alt="" />
               <img className='w-[263px] h-[116px] mt-12 ml-[34px]' src={emaar} alt="" />
               <img className='w-[263px] h-[116px] mt-12 ml-[34px]' src={dlf} alt="" />
